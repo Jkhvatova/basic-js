cb01a77ea8ba6bfa62b3b67818f85f1eac009d95
Julia Khvatova <jkhvatova@gmail.com>
2022-04-28 10:01:34 +0300

    feat: solved carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+    // remove line with error and write your code here
+  let sampleActivityToNum = Number(sampleActivity);
+  if (typeof sampleActivity !== "string" || sampleActivity === '' || sampleActivityToNum <= 0 || sampleActivityToNum >= 15 || isNaN(sampleActivityToNum)) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityToNum) / (0.693 / HALF_LIFE_PERIOD));
 }
 
 module.exports = {
