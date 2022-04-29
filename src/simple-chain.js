c5b5b0b0a372b2b7f05aa9a0bffb324dc23be0f0
Julia Khvatova <jkhvatova@gmail.com>
2022-04-29 20:54:36 +0300

    feat: solved transform-array

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,26 +5,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  str: '',
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.str.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+ addLink(value) {
+   value = '' + value;
+    this.value = '( '  + value + ' )';
+   this.arr.push(this.value);
+   return this;
+
+ },
+ removeLink(position) {
+  if(this.arr[position - 1] === undefined) {
+    this.arr = [];
+    throw new Error ('You can\'t remove incorrect link!');
   }
+  this.arr.splice(position - 1, 1);
+  this.str = this.arr.join("~~");
+ return this;
+},
+ reverseChain() {
+    this.arr.reverse();
+  return this;
+},
+ finishChain() {
+    this.str = this.arr.join("~~");
+    this.arr = [];
+    return this.str;
+ }
 };
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+ if (!Array.isArray(arr)) {
+  throw new Error("'arr' parameter must be an instance of the Array!");
+}
+let result = [...arr];
+let final = [];
+for(let i = 0; i < result.length; i++) {
+  if(result[i] === '--double-next') {
+     if (i !== result.length - 1) {
+      final.push(result[i+1]);
+    } 
+  } else if (result[i] === '--double-prev') {
+    if (i !== 0) {
+      final.push(result[i-1]);
+    }
+  } else if (result[i] === '--discard-prev') {
+    if (i !== 0) {
+      final.pop();
+    } 
+  } else if (result[i] === '--discard-next') {
+   i += 2;
+    
+  } else {
+    final.push(result[i]);
+         } 
+}
+return final;
 }
 
 module.exports = {
